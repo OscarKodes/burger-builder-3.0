611666835607f75248dc6a988ad5a9e9ebaee1c6
Oscar Ko <OscarKoEmail@gmail.com>
2019-12-11 10:48:47 -0500

    Add burger controls interface

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
 
@@ -11,10 +12,10 @@ class BurgerBuilder extends Component {
 
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         }
     }
 
@@ -22,7 +23,7 @@ class BurgerBuilder extends Component {
         return (
             <React.Fragment>
                 <Burger ingredients={this.state.ingredients} /> 
-                <div>Build Controls</div>
+                <BuildControls />
             </React.Fragment>
         )
     }
