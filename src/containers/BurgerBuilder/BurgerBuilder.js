ce670c5f7687e28ae082d798f4b46a3a2ca00749
Oscar Ko <OscarKoEmail@gmail.com>
2019-12-11 10:21:14 -0500

    Create state with ingredients for burgerbuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,25 @@ import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
 
+    // state can also be declared like this ====
+    // contructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render () {
         return (
             <React.Fragment>
-                <Burger /> 
+                <Burger ingredients={this.state.ingredients} /> 
                 <div>Build Controls</div>
             </React.Fragment>
         )
