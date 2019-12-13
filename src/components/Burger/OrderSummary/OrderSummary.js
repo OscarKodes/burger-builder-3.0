c077074b3ba874038c6d66833375b57beb381bb3
Oscar Ko <oscarkoemail@gmail.com>
2019-12-13 17:27:20 -0500

    Add shouldComponentUpdate to Modal to improve app performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,45 @@
-import React from "react";
+import React, {Component} from "react";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-    
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    // this could be a functional component
+    // declared as a class for demonstration purposes
+    componentDidUpdate () {
+        console.log("[OrderSummary] Did update");
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
                     <span style={{textTranform: 'capitalize'}}>
                         {igKey}
                     </span>
-                    : {props.ingredients[igKey]}
+                    : {this.props.ingredients[igKey]}
                 </li>
             );
         });
 
-    return (
-        <React.Fragment>
-            <h3>Your Order</h3>
-            <p>A burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button 
-                clicked={props.togglePurchase}
-                btnType="Danger">CANCEL</Button>
-            <Button 
-                clicked={props.purchaseContinue}
-                btnType="Success">CONTINUE</Button>
-        </React.Fragment>
-    );
+        return (
+            <React.Fragment>
+                <h3>Your Order</h3>
+                <p>A burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button 
+                    clicked={this.props.togglePurchase}
+                    btnType="Danger">CANCEL</Button>
+                <Button 
+                    clicked={this.props.purchaseContinue}
+                    btnType="Success">CONTINUE</Button>
+            </React.Fragment>
+        );
+    }
+    
 };
 
-export default orderSummary;
+export default OrderSummary;
